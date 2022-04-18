175c4c89f887e7a44b8a007490b12b088990ce40
PavelRoR <pavelvp2182@yandex.ru>
2022-04-18 13:56:44 +0500

    Main age is adapted

---
+++ question-3.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device=width, initial-scale=1"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="Как вы воспринимаете секс?"/>
+    <meta property="og:description" content="3 вопрос теста"/>
+    <title>«Как вы воспринимаете секс?» 3 вопрос теста</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico"/>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.png"/>
+    <link rel="stylesheet" href="css/styles.min.css"/>
+    <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?160",t.onload=function(){VK.Retargeting.Init("VK-RTRG-350620-gJCgg"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script>
+    <noscript><img src="https://vk.com/rtrg?p=VK-RTRG-350620-gJCgg" style="position:fixed; left:-999px;" alt=""/></noscript><!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async="async" src="https://www.googletagmanager.com/gtag/js?id=UA-130434463-1"></script>
+    <script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-130434463-1');</script><!-- Facebook Pixel Code -->
+    <script>!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '2406475009380573'); fbq('track', 'PageView');</script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2406475009380573&amp;ev=PageView&amp;noscript=1"/></noscript><!-- End Facebook Pixel Code -->
+  </head>
+  <body><!-- Yandex.Metrika counter -->
+    <script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter35450015 = new Ya.Metrika({ id:35450015, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script>
+    <noscript>
+      <div><img src="https://mc.yandex.ru/watch/35450015" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript><!-- /Yandex.Metrika counter -->
+<!-- Rating@Mail.ru counter -->
+    <script type="text/javascript">var _tmr = window._tmr || (window._tmr = []); _tmr.push({id: "3089960", type: "pageView", start: (new Date()).getTime(), pid: "USER_ID"}); (function (d, w, id) { if (d.getElementById(id)) return; var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id; ts.src = "https://top-fwz1.mail.ru/js/code.js"; var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);}; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "topmailru-code");</script>
+    <noscript>
+      <div><img src="https://top-fwz1.mail.ru/counter?id=3089960;js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru"/></div>
+    </noscript><!-- //Rating@Mail.ru counter -->
+    <header>
+      <div class="container"><a href="https://mastervision.su" target="_blank"><img id="logo" src="img/logo.png" alt="Логотип"/></a>
+        <div class="contacts"><a class="contact contact-tel" href="callto:8-499-346-79-46"><span>8-499-346-79-46</span></a><a class="contact contact-skype" href="https://join.skype.com/invite/gkwrZmJPOtuf"><span>support-mastervision</span></a><a class="contact contact-email" href="mailto:info@mastervision.su"><span>info@mastervision.su</span></a></div>
+      </div>
+    </header>
+    <section class="main main-question main-question-3">
+      <div class="container">
+        <h1 class="question-title">Вопрос 3</h1>
+        <h2 class="question-subtitle">Как вы воспринимаете секс?</h2>
+      </div>
+    </section>
+    <section class="second">
+      <div class="container">
+        <form class="question-form" action="question-4.php" method="POST">
+          <div class="choice">
+            <input class="q-radio" type="radio" name="question-3" id="q-choice-0" value="0" checked />
+            <label for="q-choice-0">Секс — это страсть, кайф, спонтанные эмоции, состояние эйфории и влюблённости. Особенно в начале отношений. Но по истечению некоторого времени влечение угасает и кажется, что прошла любовь</label>
+          </div>
+          <div class="choice">
+            <input class="q-radio" type="radio" name="question-3" id="q-choice-1" value="1"/>
+            <label for="q-choice-1">Сначала я хочу этого мужчину у себя в голове и сердце, а после возникает желание в теле. Часто это источник энергии. Удовольствие. Возможность выразить свои чувства к мужчине. Секс — это что-то очень сакральное и он возможен только после душевной близости</label>
+          </div>
+          <div class="choice">
+            <input class="q-radio" type="radio" name="question-3" id="q-choice-2" value="2"/>
+            <label for="q-choice-2">Секс — это необходимость в отношениях. Секс подтверждает любовь и взаимность. Помогает удержать мужчину. Секс больше нужен мужчине, а иначе он пойдет на сторону</label>
+          </div>
+          <div class="choice">
+            <input class="q-radio" type="radio" name="question-3" id="q-choice-3" value="3"/>
+            <label for="q-choice-3">Секс — это драйв и страсть. Я люблю секс, независимо от того люблю ли я мужчину. Спокойно могу заняться сексом спонтанно. Секс — это способ покорить, привязать к себе мужчину, быть для него незаменимой. А так же секс — это способ «расплатиться» с мужчиной, отблагодарить его за подарки, ухаживания, внимание</label>
+          </div>
+          <div class="choice">
+            <input class="q-radio" type="radio" name="question-3" id="q-choice-4" value="4"/>
+            <label for="q-choice-4">Люблю красивый, романтичный секс, с ухаживаниями. Секс — это проявление доверия к мужчине. Сначала мужчина должен завоевать мое сердце. Секс возможен лишь «по любви»</label>
+          </div>
+          <div class="choice">
+            <input class="q-radio" type="radio" name="question-3" id="q-choice-5" value="5"/>
+            <label for="q-choice-5">Другой вариант</label>
+          </div>
+          <?php 
+				$answer1 = $_POST['question-1'];
+$answer2 = $_POST['question-2'];
+?>
+                <input type="hidden" name="question-1" value="<?=$answer1?>" />
+                <input type="hidden" name="question-2" value="<?=$answer2?>" />
+          <button class="button form-button" type="submit">Далее</button>
+        </form>
+      </div>
+    </section>
+    <!-- AutoWebOffice: UTM or OpenStat Counter -->
+    <script type="text/javascript">var url = "https://shop.mastervision.su/?r=api/utmopenstat";</script>
+    <script type="text/javascript" src="https://shop.mastervision.su/js/utm_openstat.js" defer="defer"></script><!-- /AutoWebOffice: UTM or OpenStat Counter -->
+  </body>
+</html>
